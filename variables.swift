ffbb285fc4404f0e497a1614e0e252bdd5c4aeba
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-08-23 23:13:56 +0300

    Create variables.swift

---
+++ variables.swift
@@ -0,0 +1,11 @@
+// Вывести "Hello World" и true
+
+var a = "Hello "
+var b : String = "World"
+var c : Float = 3.1511
+var d: Float = 1.1357
+var e: Bool = c > d
+var f: Int = 5
+var g: Double = 1.25678990
+print(a + b)
+print(e)
